e7ba5fe965bbcaa29acd38a958ea23f120276e8b
skipfromua <skipfromua@gmail.com>
2019-12-05 16:17:55 +0200

    "second"

--- bot_handlers.py
+++ bot_handlers.py
@@ -30,9 +30,10 @@ def enter_age(message, our_db_table):
             users_db.update({ 'age': our_db_table['age'] }, { "$set": { "age": age}})
             main_menu(message)
 
-        bot.send_message(message.chat.id, 'Вы ввели невалидный возраст, попробуйте снова:')
+
 
         if not our_db_table['age']:
+            bot.send_message(message.chat.id, 'Вы ввели невалидный возраст, попробуйте снова:')
             return
 
     if not our_db_table['gender']:
@@ -70,10 +71,11 @@ def main_menu(message):
 
 
 def change_name(message, our_db_table):
+    keyboard = types.ReplyKeyboardMarkup()
     our_db = our_db_table.copy()
     our_db['name'] = None
     users_db.update(our_db_table, { "$set": our_db })
-    bot.send_message(message.chat.id, 'Введите имя:')
+    bot.send_message(message.chat.id, 'Введите имя:', reply_markup=keyboard)
 
 
 def change_age(message, our_db_table):
