479b4c3bac744bc9d010ea8f0f750752acfe1891
skipfromua <skipfromua@gmail.com>
2019-12-05 16:22:51 +0200

    "second"

--- bot_handlers.py
+++ bot_handlers.py
@@ -72,7 +72,7 @@ def main_menu(message):
 
 def change_name(message, our_db_table):
     keyboard = types.ReplyKeyboardMarkup()
-    keyboard.row()
+    keyboard.row('')
     our_db = our_db_table.copy()
     our_db['name'] = None
     users_db.update(our_db_table, { "$set": our_db })
