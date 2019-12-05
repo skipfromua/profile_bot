0a627a931bf18f19d1562e1e6c8a720f408503bd
skipfromua <skipfromua@gmail.com>
2019-12-05 11:21:38 +0200

    "second"

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
 import pymongo
 
-client = pymongo.MongoClient('mongodb://localhost/tutorial_bot')
+client = pymongo.MongoClient('mongodb://pacific-thicket-10824.herokuapp.com/tutorial_bot')
 users_db = client.get_database()["users_db"]
